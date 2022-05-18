db11cf02ba41d4d28c96db75ac8145634bdc1aa8
joey57 <njorogejoey5@gmail.com>
2022-05-18 14:47:23 +0300

    design dash

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,11 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for, abort,flash
 from . import main
+from flask_login import login_required, current_user
+from datetime import datetime
+
+
+@main.route('/')
+def index():
+    '''
+    '''
+    return render_template('index.html')

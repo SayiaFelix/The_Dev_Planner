bda9386340918b21a18ec7f08e053193e3b5bb13
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-18 15:29:30 +0300

    styling

---
+++ app/auth/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError
+from wtforms.validators import DataRequired,Email,EqualTo
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email Address ::',validators=[DataRequired(),Email()])
+    author = StringField('Enter authors name ::',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('Email exists already.')
+
+    def validate_author(self,data_field):
+        if User.query.filter_by(author = data_field.data).first():
+            raise ValidationError('Author name is taken already.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email Address ::',validators=[DataRequired(),Email()])
+    password = PasswordField('Password ::',validators =[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')

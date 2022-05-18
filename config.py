c6b1697b7258007935f7fa15242f28e62357b3ed
joey57 <njorogejoey5@gmail.com>
2022-05-18 14:30:12 +0300

    design dashboard

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+  '''
+  '''
+  # SECRET_KEY = '1234'
+  pass
+
+class ProdConfig(Config):
+  '''
+  '''
+  pass
+
+
+class DevConfig(Config):
+  '''
+  '''
+  DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

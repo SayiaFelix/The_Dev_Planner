b446514d0e7eec2fff411a10550aa8d787289b5a
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-19 09:33:38 +0300

    styling

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,10 +4,10 @@ from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email Address ::',validators=[DataRequired(),Email()])
-    author = StringField('Enter authors name ::',validators = [DataRequired()])
-    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
+    email = StringField('Email Address:',validators=[DataRequired(),Email()])
+    author = StringField('Enter Name:',validators = [DataRequired()])
+    password = PasswordField('Password:',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password:',validators = [DataRequired()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self,data_field):
@@ -19,7 +19,7 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Author name is taken already.')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email Address',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators =[DataRequired()])
+    email = StringField('Email Address:',validators=[DataRequired(),Email()])
+    password = PasswordField('Password:',validators =[DataRequired()])
     remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Login')
